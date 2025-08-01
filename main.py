2eb43eb19cf530c72210490fc1c0134e49ba9203
Archiet <archiettanwar71@gmail.com>
2025-08-01 12:57:34 +0530

    complete3

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ df=pd.read_csv("topics.csv")
 pdf=FPDF(orientation="P",unit="mm",format="A4")
 pdf.set_auto_page_break(auto=False,margin=0)
 for i,j in df.iterrows():
+    diff=20
     title=j["Topic"]
 
     pdf.add_page()
@@ -18,12 +19,20 @@ for i,j in df.iterrows():
     pdf.set_text_color(180,180,180)
     pdf.cell(w=0,h=10,txt=title,align="R")
 
+    for lines in range(30):
+        diff+=10
+        pdf.line(10,y1=diff,x2=200,y2=diff)
+
     for pages in range(j["Pages"]-1):
+        diff=0
         pdf.add_page()
         pdf.ln(274)
         pdf.set_font(family="Times", style="B", size=10)
         pdf.set_text_color(180,180,180)
         pdf.cell(w=0,h=10,txt=title,align="R")
+        for lines in range(30):
+            diff += 10
+            pdf.line(10, y1=diff, x2=200, y2=diff)
 
 
 pdf.output("output.pdf")
